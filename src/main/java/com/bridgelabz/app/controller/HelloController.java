066842a60466a4e785d55a36bf686803cba1a47a
surajseth90 <surajseth90@gmail.com>
2021-06-18 14:34:44 +0530

    Added code to use put request

--- src/main/java/com/bridgelabz/app/controller/HelloController.java
+++ src/main/java/com/bridgelabz/app/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.bridgelabz.app.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,4 +35,8 @@ public class HelloController {
 		return "Hello " + user.getFirstName() + " " + user.getLastName() + "!!";
 	}
 
+	@PutMapping("/put/{firstName}")
+	public String sayHelloPut(@PathVariable String firstName, @RequestParam(value= "lastName") String lastName) {
+		return "Hello" + firstName + " " + lastName + "!";
+	}
 }
