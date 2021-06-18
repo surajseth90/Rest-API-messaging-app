333254aa437bf38da7a559f4bc12f9d7cce6afa7
surajseth90 <surajseth90@gmail.com>
2021-06-18 09:57:43 +0530

    Added code to use get request method and pass name as query parameter

--- src/main/java/com/bridgelabz/app/controller/HelloController.java
+++ src/main/java/com/bridgelabz/app/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.app.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,12 @@ public class HelloController {
 	@RequestMapping(value = {"","/","/home"})
 	public String sayHello() {
 		return "Hello";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = {"/query"})
+	public String sayHello(@RequestParam(value = "name") String name) {
+		return "Hello "+name+"!";
+		
 	}
 
 }
