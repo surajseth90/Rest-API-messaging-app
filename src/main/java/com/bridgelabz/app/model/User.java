0b8a0313f71ac298861b09e2c1428343e39916c6
surajseth90 <surajseth90@gmail.com>
2021-06-18 14:32:20 +0530

    Added code to use post request and create DOT bean

---
+++ src/main/java/com/bridgelabz/app/model/User.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.app.model;
+
+public class User {
+
+	private String firstName = "mark";
+	private String lastName = "taylor";
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
